3ceae3a4ec92215ba35e91a7c042d1b17687158c
MASKUN2 <maskunflower2@naver.com>
2026-05-03 15:54:41 +0900

    chore: enable stable Page serialization mode VIA_DTO

--- src/main/kotlin/com/quietchatter/talk/TalkApplication.kt
+++ src/main/kotlin/com/quietchatter/talk/TalkApplication.kt
@@ -4,8 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.web.config.EnableSpringDataWebSupport
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
 import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
